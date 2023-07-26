592234e8d6eee77a11fcd991111551fef38cc7fb
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2023-07-26 22:40:51 +0530

    Add files via upload

---
+++ SwitchCaseExample.java
@@ -0,0 +1,33 @@
+public class SwitchCaseExample {
+    public static void main(String[] args) {
+        int day = 3;
+        switch (day){
+            case 1 :
+                System.out.println("MONDAY");
+                break;
+            case 2 :
+                System.out.println("TUESDAY");
+                break;
+            case 3 :
+                System.out.println("WEDNESDAY");
+                break;
+            case 4 :
+                System.out.println("THURSDAY");
+                break;
+            case 5 :
+                System.out.println("FRIDAY");
+                break;
+                case 6 :
+                System.out.println("SATURDAY");
+                break;
+                case 7 :
+                System.out.println("SUNDAY");
+                break;
+
+            default:
+                System.out.println("Invalid");
+
+
+        }
+    }
+}
