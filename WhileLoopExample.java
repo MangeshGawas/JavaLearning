aeba1d4ecbbfa61dd989f9e152100752c6a8cc2e
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2023-07-26 22:02:41 +0530

    Add files via upload

---
+++ WhileLoopExample.java
@@ -0,0 +1,9 @@
+public class WhileLoopExample {
+    public static void main(String[] args) {
+        int i = 0;
+        while(i<=10){
+            System.out.println(i+"here is the i value that are from while loop ");
+            i++;
+        }
+    }
+}
