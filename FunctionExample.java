22d8b8bacff050434f595541087ac326872f65a5
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2023-07-26 22:07:50 +0530

    Add files via upload

---
+++ FunctionExample.java
@@ -0,0 +1,12 @@
+public class FunctionExample {
+    public static void main(String[] args) {
+        greet();
+        privateGreet();
+    }
+    public static void greet(){
+        System.out.println("Hello Coder!!!");
+    }
+    private static void privateGreet(){
+        System.out.println("Hello Coder!!! this is private greet it can't be goes outside");
+    }
+}
