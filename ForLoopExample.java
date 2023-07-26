b5d4536215e9201c50d17d77f94acf618d5fb7e5
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2023-07-26 21:57:48 +0530

    Add files via upload

---
+++ ForLoopExample.java
@@ -0,0 +1,7 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        for(int i = 0 ; i<=10; i++){
+            System.out.println(i+"Here is that number from above loop");
+        }
+    }
+}
