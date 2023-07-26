c1a892f8a18a81e6d5554d5b9880c08e23e0eb5d
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2023-07-26 21:49:20 +0530

    Add files via upload

---
+++ VariablesExample.java
@@ -0,0 +1,15 @@
+public class VariablesExample {
+    public static void main(String[] args) {
+        int age = 23;
+        double height = 1.67;
+        String name = "Mangesh";
+        char grade = 'A'; //This should be in single code
+        boolean isProgrammer = true;
+
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(name);
+        System.out.println(grade);
+        System.out.println(isProgrammer);
+    }
+}
