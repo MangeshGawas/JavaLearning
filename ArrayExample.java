5b435acc1aa302326e1741ede745bceda31cdd80
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2023-07-26 22:16:08 +0530

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,7 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int [] numberArray = {1,2,3,4,5,6,7,8,9,10};
+        System.out.println(numberArray[7]);
+        System.out.println(numberArray[0]); //this will give you element from 0th index
+    }
+}
