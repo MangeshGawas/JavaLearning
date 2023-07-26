5f21210ea6906008215c9434fb1ae084063e9d0c
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2023-07-26 21:36:09 +0530

    Add files via upload
    HelloWorld code

--- Main.java
+++ Main.java
@@ -1,16 +1,5 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner takeNumber = new Scanner(System.in);
-       int a = takeNumber.nextInt();
-       int b = takeNumber.nextInt();
-       int Ans = Sum(a,b);
-        System.out.println(Ans);
-    }
-    private static int Sum(int a ,int b){
-
-         int sum = a + b;
-         return sum;
-
+        System.out.println("Hello Coder");
     }
 }
