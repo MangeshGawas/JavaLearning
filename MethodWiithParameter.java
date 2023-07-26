27f21aa5746dc1271bdcab933ec6a1836f7eb155
deCoder007 <70948059+MangeshGawas@users.noreply.github.com>
2023-07-26 22:11:58 +0530

    Add files via upload

---
+++ MethodWiithParameter.java
@@ -0,0 +1,11 @@
+import java.sql.SQLOutput;
+
+public class MethodWiithParameter {
+    public static void main(String[] args) {
+        greet("Mangesh");
+    }
+
+    public static void greet(String name){
+        System.out.println("Hello "+ name);
+    }
+}
